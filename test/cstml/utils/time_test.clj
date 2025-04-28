40882e0845cee95859e4c4ae57b19ba3058cc1bc
Vlad P. Luchian <cstmlcodes@gmail.com>
2025-04-28 21:20:13 +0100

    [update] add test.

--- test/cstml/utils/time_test.clj
+++ test/cstml/utils/time_test.clj
@@ -1,8 +1,9 @@
 (ns cstml.utils.time-test
   (:require [cstml.utils.time :as t]
-            [clojure.test :refer [is deftest]]
-            [clojure.spec.gen.alpha :as gen]
-            [clojure.spec.alpha :as s]))
+            [clojure.test :refer [is deftest testing]]
+            [clojure.spec.alpha :as s]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check :as check]))
 
 (deftest instantiation
   (is (t/instant))
@@ -12,6 +13,12 @@
   (is #time/instant "2025-04-28T19:47:07.288456043Z")
   (is #time/local-date "2025-04-28"))
 
-(deftest spec
-  (mapv #(is (s/assert ::t/instant %)) (gen/sample (s/gen ::t/instant)))
-  (mapv #(is (s/assert ::t/local-date %)) (gen/sample (s/gen ::t/local-date))))
+(deftest generator-spec-and-parser
+  (let [can-parse (prop/for-all
+                   [instant (s/gen ::t/instant)
+                    local-date (s/gen ::t/local-date)]
+                   (testing "can parse instant"
+                     (is (t/parse-instant (str instant)) ))
+                   (testing "can parse local-date"
+                     (is (t/parse-local-date (str local-date)) )))]
+    (check/quick-check 100 can-parse)))
